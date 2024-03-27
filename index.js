99bc283eafd8b2f37d1f29093689012a8824c941
Francis-2006 <161254920+Francis-2006@users.noreply.github.com>
2024-03-28 05:33:20 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+
+
+function myfunc(){
+    fname=document.getElementById('inputName').value ;
+    age=document.getElementById('inputAge').value ;
+    area=document.getElementById('inputArea').value ;
+    gender=document.getElementById('inputGender').value ;
+    
+    document.querySelector('.outputcont').classList.toggle("open");
+   
+   
+ 
+    if(age>=18){
+        document.getElementById('output').innerHTML="QUALIFIED✅"
+        document.getElementById('output').style.color="white"
+        document.getElementById('output').style.background="green"
+        document.getElementById('p1').innerHTML=`Hello ${fname} you are ${age} years,you are eligible to vote`;
+        document.querySelector('.close').style.marginTop='-125px'
+        document.getElementById('output').style.marginTop="20px"
+
+
+
+    } else{
+        document.getElementById('output').innerHTML="UNQUALIFIED"
+        document.getElementById('output').style.color="white"
+        document.getElementById('output').style.background="red"
+        document.getElementById('output').style.marginTop="60px"
+        document.getElementById('output').style.position="relative"
+        document.querySelector('.close').style.marginTop='-115px'
+
+}
+
+
+
+
+
+if(!age,!area,!gender.trim()){
+    alert("please in the blank space")
+    document.querySelector('.outputcont').style.display="none"
+    event.preventDefault();
+}else{
+    document.querySelector('.outputcont').style.display="block"
+
+}
+
+if(fname==""){
+    alert("please enter your name")
+}
+
+}
+
+
+
